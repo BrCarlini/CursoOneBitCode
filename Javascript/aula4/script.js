b1781d46f3f2d5fa19ace65cc6015e0316afe7d2
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-08 19:13:15 -0300

    Aula 4 e 5 Assistida -  Operadores Lógico e  de comparação

---
+++ Javascript/aula4/script.js
@@ -0,0 +1,18 @@
+// Comparação de dados
+
+let spaceship = 'Golias'
+let velocity = 80
+
+console.log(velocity == 80)
+console.log(velocity >= 130)
+
+/* 
+== Igual que 
+=== Restritamente igual
+> maior que 
+< menor que 
+>= maior igual que
+<= menor igual que
+!= diferente de
+!== Restritamente diferente de
+*/
---
+++ Javascript/aula5/script.js
@@ -0,0 +1,18 @@
+/* 
+Tabela Verdade do && (AND)
+true && true = true
+true && false = false
+false && true = false
+false && false = false
+
+Tabela Verdade do ││ (OR)
+true ││ true = true
+true ││ false = true
+false ││ true = true
+false ││ false = false
+
+
+Tabela Verdade ! (NOT)
+!true = false
+!false = true
+*/
