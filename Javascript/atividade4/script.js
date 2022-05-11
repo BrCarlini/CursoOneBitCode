70787bb0cd9f5e494581562087ccd39fdb58affd
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-10 22:59:01 -0300

    ...

--- Javascript/atividade4/script.js
+++ Javascript/atividade4/script.js
@@ -2,8 +2,8 @@ let anosLuz = prompt("Informe a distância em anos luz: ")
 
 let opcao = parseInt(prompt("Escolha uma opção de conversão:\n1 - Parsec(pc)\n2 - Unidade astronônima(AU)\n3 - Quilômetros(km)"))
 
-let nomeUnidade = " "
-let operacao = 0
+let nomeUnidade
+let operacao
 
 switch(opcao){
     case 1:
@@ -12,7 +12,7 @@ switch(opcao){
         alert(`A distância em Anos-luz escolhida foi de ${anosLuz} convertida em ${nomeUnidade} = ${operacao}`)
         break
     case 2:
-        nomeUnidade = "Unidade astronônima(AU)"
+        nomeUnidade = "Unidade Astronônima(AU)"
         operacao = anosLuz * 63241.1
         alert(`A distância em Anos-luz escolhida foi de ${anosLuz} convertida em ${nomeUnidade} = ${operacao}`)
         break
