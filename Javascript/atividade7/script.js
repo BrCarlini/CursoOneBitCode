63808e5ee8f578fa359bfe905f5293d2a08db567
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-12 17:44:11 -0300

    Atividade 7 iniciada

---
+++ Javascript/atividade7/script.js
@@ -0,0 +1,9 @@
+let nomeNave = "Bruno"//prompt("Informe o nome da nava: ")
+let nomeInvertido = " "
+
+for(i = 0; i <= nomeNave.length; i++){
+    var separar = nomeNave.replace()
+    
+}
+
+console.log(nomeNave[i])
