94ec47ccc7f80b8e23406039333d7dc4387e4143
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-18 21:23:47 -0300

    Aulas assistidas

---
+++ Javascript/aula22/script.js
@@ -0,0 +1,8 @@
+let hitchedSpaceships = ["Supernova", "Elemental", "Helmet"]
+console.log(hitchedSpaceships[0])
+
+let newSpaceships = ["Supernova", 7, true]
+console.log(newSpaceships[1])
+
+let newList = new Array(5)
+console.log(newList)
---
+++ Javascript/aula23/script.js
@@ -0,0 +1,21 @@
+let hitchedSpaceships = ["Colossus", "Elemental", "Helmet"]
+console.log(hitchedSpaceships)
+
+
+hitchedSpaceships.push("Supernova")
+console.log(hitchedSpaceships)
+
+
+hitchedSpaceships.pop()
+console.log(hitchedSpaceships)
+
+hitchedSpaceships.shift()
+console.log(hitchedSpaceships)
+
+
+hitchedSpaceships.unshift("Fenix")
+console.log(hitchedSpaceships)
+
+console.log(hitchedSpaceships.length)
+
+console.log(hitchedSpaceships.indexOf("Helmet"))
