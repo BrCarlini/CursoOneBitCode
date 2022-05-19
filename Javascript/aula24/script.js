2b1b264e7367fa862565b4ee61f6d818879fa218
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-19 16:27:05 -0300

    Aula 24 assistida

---
+++ Javascript/aula24/script.js
@@ -0,0 +1,5 @@
+let spaceshipNames = ["Elemental", "Darwin", "Ártemis", "Supernova"]
+
+let removedSpaceships = spaceshipNames.splice(1, 2, "Deméter", "Puller", "Golias")
+
+console.log(spaceshipNames)
