8d7383554de6d21c4eed4825d5380d5e0c94555a
Bruno Carlini <bruno-carlini@hotmail.com>
2023-10-26 19:15:23 -0300

    Conectando MongoDB com Express, criado Models

--- NodeJs/aula7-to-do-list-express/src/routes/checklist.js
+++ NodeJs/aula7-to-do-list-express/src/routes/checklist.js
@@ -2,24 +2,55 @@ const express = require('express');
 
 const router = express.Router();
 
+const Checklist = require('../models/checklist')
+
+
 // Pega todos da rota
-router.get('/', (req, res) => {
-    console.log('Olá')
-    res.send();
+// Async - Para informar que é uma função assincrona
+router.get('/', async (req, res) => {
+
+    try{
+        // Await - Aguarda para buscar tudo do database
+        let checklists = await Checklist.find({})
+        res.status(200).json(checklists)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+
 })
 
+
+
 // Adiciona o que foi passado no body da req e devolve em formato json
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json(req.body);
+router.post('/', async (req, res) => {
+    let { name } = req.body
+    
+
+    try{
+        let checklist = await Checklist.create({ name })
+        res.status(200).json(checklist)
+    } catch(error) {
+        res.status(422).json(error)
+    }
+
 })
 
 
 // Pega o item(objeto) referente ao ID passado/informado no caminho
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    
+    try{
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    }catch(error){
+        res.status(422).json(error)
+    }
+
 })
+
+
 
 router.put('/:id', (req, res) => {
     res.send(`PUT ID ${req.params.id}`)
