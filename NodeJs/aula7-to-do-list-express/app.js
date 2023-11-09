b9669af0462995bbd1c260c7ddf49fdbe02551b8
Bruno Carlini <bruno-carlini@hotmail.com>
2023-11-09 10:16:43 -0300

    NodeJs: Arquivos modificados, routes, views, public e app

--- NodeJs/aula7-to-do-list-express/app.js
+++ NodeJs/aula7-to-do-list-express/app.js
@@ -1,6 +1,17 @@
 // Importa o framework Express
 const express = require('express');
+
+// Importa o modulo/biblioteca Path para trabalhar com arquivos e diretórios
+const path = require('path')
+
+// Importa a rota que queros utilizar
+// Rota checklist
 const checkListRouter = require('./src/routes/checklist')
+
+// Rota index
+const rootRouter = require('./src/routes/index')
+
+// Importa o database 
 require('./config/database');
 
 // Chama a função express(), para criar aplicação/ rotas Express
@@ -8,7 +19,22 @@ const app = express();
 
 // Para usar Middlewares basta chamar o .use() e dentro dele chamar a verificação desejada
 // Nesse exemplo, é chamada a .json() onde verifica se existe algum json e se deve processar esse json, deixando no body da req
-app.use(express.json());
+//app.use(express.json());
+
+
+
+
+
+// Configura onde as Views vão ficar, passando no 2°arg o path.join para juntar duas rotas
+// __dirname diretorio atual
+// src/views pasta onde vai estar a views
+app.set('views', path.join(__dirname, 'src/views'))
+
+// Informa que os arquivos estaticos vao estar no diretorio public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Instala a view engine
+app.set('view engine', 'ejs')
 
 /*
 const log = (req, res, next) => {
@@ -22,19 +48,14 @@ app.use(log)
 */
 
 
-// Usando arquivo de rotas como se fosse Middlewares
-app.use('/checklists',checkListRouter)
+// Usando/importando arquivo de rotas(Router) como Middlewares
+app.use('/checklists', checkListRouter)
+
+app.use('/', rootRouter)
 
 
-// .get() Cria uma rota
 
-/* 
-
-1° arg: caminho/rota
-2° arg: arrow function passando dois params, um de requisição e outro de resposta
-
-*/
-
+// Porta que nossa aplicação esta disponivel
 app.listen(3000, () => {
     console.log('Servidor foi iniciado')
 })
--- NodeJs/aula7-to-do-list-express/src/routes/checklist.js
+++ NodeJs/aula7-to-do-list-express/src/routes/checklist.js
@@ -51,15 +51,27 @@ router.get('/:id', async (req, res) => {
 })
 
 
-
-router.put('/:id', (req, res) => {
-    res.send(`PUT ID ${req.params.id}`)
+// Atualiza o item informando o ID 
+router.put('/:id', async (req, res) => {
+    let {name} = req.body
+    try{
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
+        res.status(200).json(checklist)
+    }catch(error){
+        res.status(422).json(error)
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    res.send(`DELETE ID ${req.params.id}`)
+
+// Deleta o item informando o ID
+router.delete('/:id', async (req, res) => {
+    try{
+        let checklist = await Checklist.findByIdAndRemove(req.params.id)
+        res.status(200).json(checklist)
+    }catch(error){
+        res.status(422).json(error)
+    }
 })
 
-router.delete('')
 
 module.exports = router;
